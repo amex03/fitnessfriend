2fadff81114cb416cd1c13f0623d24881c57d4fd
Camsca <camila_vass@hotmail.com>
2023-11-17 15:16:05 -0800

    Establishing relationships with models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,25 @@
 const User =require ('./User')
-const Dashboard = requiere ('./dashboard')
+const Dashboard = requiere ('./dasboard')
 const Comment =require ('./comment')
+
+
+Dashboard.belongTo(User,{
+    foreignKey:'user_name',
+    targekey:'username'
+});
+
+Comment.belongsTo(User,{
+    foreignKey:'user_id',
+    targetKey:'id'
+});
+
+Dashboard.hasMany(Comment,{
+foreignKey:'dashboard_id',
+sourcekey:'id',
+});
+
+module.exports={
+    User,
+    Dashboard,
+    Comment
+};
