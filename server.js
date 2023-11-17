e81cb3760893e345e0ec048760ca87659f8eda0d
Camsca <camila_vass@hotmail.com>
2023-11-17 15:32:10 -0800

    builind server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');//session to do
+const exphbs = require('express-handlebars');//handlebars to do
+const routes = require('./controllers');//routes to do
+const sequelize = require('./config/connection');//sequelize to do
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const sess={
+    secret:'Super secret secret',
+    resave:false,
+    saveUninitialized:true,
+};
+
+// app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));//todo public
+
+app.use(routes);//todo routes
+
+sequelize.sync({force:false}).then(()=>{
+    app.listen(PORT,()=>console.log('Now listening'));
+});
