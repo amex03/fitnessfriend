9d9d8a6ae043c4ed186ce9e10cec537af22dc8d9
Camsca <camila_vass@hotmail.com>
2023-11-19 15:51:47 -0800

    postThreshold limit post views for non logged in users

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,11 @@
 const{Dashboard, Comment} = require('../models');
+const { post } = require('../routes');
 
 const HomepageController = {
     getHomepage: async(req,res)=>{
         try{
-            const dashboardData = await Dashboard.findAll({
+            const isLogged = req.session.loggedIn;
+              const dashboardData = await Dashboard.findAll({
                 include:[{
                     model:Comment,
                     attributes:['content','date_created'],
@@ -12,6 +14,11 @@ const HomepageController = {
             const dashboards = dashboardData.map((dashboard) =>
             dashboard.get({plain:true})
             );
+         const postThreshold = 4;
+            if (!isLogged && dashboards.length > postThreshold){
+                res.redirect('/login');
+                return;
+            } 
             res.render('homepage',{
                 dashboards,
                 loggedIn:req.session.loggedIn,
