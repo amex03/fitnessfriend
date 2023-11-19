42198dded4115f0ffa4c471073cb4c73205b4831
Camsca <camila_vass@hotmail.com>
2023-11-19 15:32:08 -0800

    homePage Controller-getHomePage method & created utils folder wit auth

--- controllers/home.js
+++ controllers/home.js
@@ -1,1 +1,28 @@
-const{}
+const{Dashboard, Comment} = require('../models');
+
+const HomepageController = {
+    getHomepage: async(req,res)=>{
+        try{
+            const dashboardData = await Dashboard.findAll({
+                include:[{
+                    model:Comment,
+                    attributes:['content','date_created'],
+                }],
+            });
+            const dashboards = dashboardData.map((dashboard) =>
+            dashboard.get({plain:true})
+            );
+            res.render('homepage',{
+                dashboards,
+                loggedIn:req.session.loggedIn,
+            });
+
+        }catch(err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+};
+
+
+module.exports = HomepageController;
