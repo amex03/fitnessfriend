6b89d12bee504d3d7a31878d207d6eda6822cd6b
Camsca <camila_vass@hotmail.com>
2023-11-19 19:26:05 -0800

    dashboard controllers -create dashboard

--- controllers/dasboard.js
+++ controllers/dasboard.js
@@ -1,1 +1,20 @@
-c
+const {Dashboard} = require('../models')
+//create dashboard
+const DashboardController = {
+    createDashboard: async (req, res) => {
+        try{
+        const dbDashboardData = await Dashboard.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_name: req.body.user_name,
+            date_created: req.body.date_created,
+        });
+            res.json(newDashboard);
+        }catch(err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+        };
+
+        module.exports = DashboardController;
