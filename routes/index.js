b2df49f06a718dcf8460beb1a158c58c3616236a
Camsca <camila_vass@hotmail.com>
2023-11-21 16:08:14 -0800

    refractoring controllers and routes

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const HomepageController = require('../controllers/homepageController');
-// get request to fetch homepafe data
-router.get('/', HomepageController.getHomepage);
-
-module.exports = router;
